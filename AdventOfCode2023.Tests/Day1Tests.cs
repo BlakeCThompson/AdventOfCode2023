b21c1e4312da31ae7721d88830d927a1dbd1a783
Blake Thompson <blake.thompson@unicity.com>
2023-12-30 16:43:57 -0700

    update gitignore. Add another test

--- AdventOfCode2023.Tests/Day1Tests.cs
+++ AdventOfCode2023.Tests/Day1Tests.cs
@@ -16,5 +16,18 @@ namespace AOC2023.Tests
 			// Assert
 			Assert.AreEqual("142", result);
 		}
+		
+		[TestMethod]
+		public void TestDay1B()
+		{
+			// Arrange
+			Day1B day1B = new Day1B();
+
+			// Act
+			string result = day1B.calculateTest();
+
+			// Assert
+			Assert.AreEqual("281", result);
+		}
 	}
 }
